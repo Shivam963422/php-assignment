1672607ef9031e3a31fe2fd77418ef0d8d8de429
shivamkumar <shivam96342@gmail.com>
2022-06-01 02:44:21 +0530

    Added files and code for php assignment.

---
+++ Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace assignment\php;
+
+class Index
+{
+	public function indexFunction()
+	{
+		echo "Hello world!\n";
+	}
+}
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'Index.php';
+
+use assignment\php\Index as indexClass;
+
+$index = new indexClass();
+
+$index->indexFunction();
